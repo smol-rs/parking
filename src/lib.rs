d757fd126044f4ee66ae55b091e43b507f453587
jtnunley <jtnunley01@gmail.com>
2022-10-03 13:15:33 -0700

    Replace cfg(loom) with one that includes the feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,10 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
-#[cfg(not(loom))]
+#[cfg(not(all(loom, feature = "loom")))]
 use std::sync;
 
-#[cfg(loom)]
+#[cfg(all(loom, feature = "loom"))]
 use loom::sync;
 
 use std::cell::Cell;
